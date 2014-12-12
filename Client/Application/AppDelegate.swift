f57cb3efe445cddeebeb453deed6d8452c7eb48a
Richard Newman <rnewman@twinql.com>
2014-12-12 09:48:23 +0000

    TESTING: getting access to the bookmarks UI again.

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -12,9 +12,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         self.window.backgroundColor = UIColor.whiteColor()
 
-        self.window.rootViewController = BrowserViewController()
+        var accountManager: AccountManager!
+        let account: Account = MockAccount()
+
+        accountManager = AccountManager(
+            loginCallback: { account in
+                // Show the tab controller once the user logs in.
+                //self.showTabBarViewController(account)
+            },
+            logoutCallback: { account in
+                // Show the login controller once the user logs out.
+                //self.showLoginViewController(accountManager)
+        })
+
+        self.showTabBarViewController(account)
+
 
         self.window.makeKeyAndVisible()
         return true
+    }
+
+    func showTabBarViewController(account: Account) {
+        let tabBarViewController = TabBarViewController(nibName: "TabBarViewController", bundle: nil)
+        tabBarViewController.account = account
+        self.window.rootViewController = tabBarViewController
+    }
+
+    func showLoginViewController(accountManager: AccountManager) {
+        let loginViewController = LoginViewController()
+        loginViewController.accountManager = accountManager
+        self.window.rootViewController = loginViewController
     }
 }
